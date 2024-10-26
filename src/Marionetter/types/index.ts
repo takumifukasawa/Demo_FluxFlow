691532d32cbbfecdc2745a5a9302862535a2dd57
takumifukasawa <takumifukasawa@gmail.com>
2024-10-26 16:18:55 +0900

    remove const

--- src/Marionetter/types/index.ts
+++ src/Marionetter/types/index.ts
@@ -94,14 +94,16 @@ export type MarionetterTransformInfo = NeedsShorten extends true
 //
 
 // unityに合わせる
-export const enum MarionetterTrackInfoType {
-    None,
-    AnimationTrack = 1,
-    LightControlTrack = 2,
-    ActivationControlTrack = 3,
-    MarkerTrack = 4,
-    ObjectMoveAndLookAtTrack = 5,
-}
+export const MarionetterTrackInfoType = {
+    None: 0,
+    AnimationTrack: 1,
+    LightControlTrack: 2,
+    ActivationControlTrack: 3,
+    MarkerTrack: 4,
+    ObjectMoveAndLookAtTrack: 5,
+} as const;
+
+export type MarionetterTrackInfoType = (typeof MarionetterTrackInfoType)[keyof typeof MarionetterTrackInfoType];
 
 export type MarionetterTrackInfoBase = NeedsShorten extends true
     ? {
